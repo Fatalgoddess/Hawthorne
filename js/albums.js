d824c2a9bad46118deafa49fdca3967815d07d19
Antonia Ivanova <antonia.ivanova09@yahoo.com>
2015-12-19 12:23:32 +0100

    png

--- js/albums.js
+++ js/albums.js
@@ -1,67 +1,67 @@
 var pictures = [
 img1 = { 
-	src: "images/img_1.PNG",
+	src: "images/img_1.png",
 	id: "img_1",
 	description: "All gathered up"},
 img2 = {
-	src: "images/img_2.PNG",
+	src: "images/img_2.png",
 	id: "img_2",
 	description: "Final destination" },
 img3 = {
-	src: "images/img_3.PNG",
+	src: "images/img_3.png",
 	id: "img_3",
 	description: "Fatherly love"},
 img4 = {
-	src: "images/img_4.PNG",
+	src: "images/img_4.png",
 	id: "img4",
 	description: "Adelle at yoga"},
 img5 = {
-	src: "images/img_5.PNG",
+	src: "images/img_5.png",
 	id: "img5",
 	description: "Cozy by the fire"},
 img6 = {
-	src: "images/img_6.PNG",
+	src: "images/img_6.png",
 	id: "img6",
 	description: "Creamy goodness"},
  img7 = {
-	src: "images/img_7.PNG",
+	src: "images/img_7.png",
 	id: "img7",
 	description: "Just married"},
  img8 = {
-	src: "images/img_8.PNG",
+	src: "images/img_8.png",
 	id: "img8",
 	description: "Friday night"}	 ]
 
 	var profile_pictures = [
 img1 = { 
-	src: "images/skylar.PNG",
+	src: "images/skylar.png",
 	id: "img_1",
 	description: "Skylar Grey"},
 img2 = {
-	src: "images/Harley.PNG",
+	src: "images/Harley.png",
 	id: "img_2",
 	description: "Harley Hawthorne" },
 img3 = {
-	src: "images/Hana.PNG",
+	src: "images/Hana.png",
 	id: "img_3",
 	description: "Hana"  },
 img4 = {
-	src: "images/Ezikiel.PNG",
+	src: "images/Ezikiel.png",
 	id: "img_4",
 	description: "Ezikiel"  },
 img5 = {
-	src: "images/Reece.PNG",
+	src: "images/Reece.png",
 	id: "img_5",
 	description: "Reece"  },
 img6 = {
-	src: "images/Celest.PNG",
+	src: "images/Celest.png",
 	id: "img_6",
 	description: "Celest"  },
 img7 = {
-	src: "images/Gabriel.PNG",
+	src: "images/Gabriel.png",
 	id: "img_7",
 	description: "Gabriel"  },
 img8 = {
-	src: "images/Isaiah.PNG",
+	src: "images/Isaiah.png",
 	id: "img_8",
 	description: "Isaiah"  }]
